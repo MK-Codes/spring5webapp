e1da0ef84f80bcd6de5af372231dfa95b8432fec
mk <mk@mk-codes.co.uk>
2022-06-22 10:19:34 +0100

    Creating Author and Book POJOs

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Author() {
+
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+}
